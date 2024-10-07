e63e47607380de089f6c50e89fecdc69c945ae13
kahnu044 <kahnucharan044@gmail.com>
2024-10-07 23:06:19 +0530

    Template Updated

--- pages/_meta.js
+++ pages/_meta.js
@@ -1,3 +1,4 @@
 export default {
-    index: 'Introduction'
+    index: "Introduction",
+    wordpress: "WordPress"
 }
---
+++ pages/wordpress/_meta.js
@@ -0,0 +1,4 @@
+export default {
+    plugins: "Plugins",
+    themes: "Themes"
+}
